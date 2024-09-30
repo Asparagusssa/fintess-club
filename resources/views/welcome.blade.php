4b057aa05d583fdd5139cefba6b2e0c113c89366
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2024-09-30 23:07:41 +0400

    1 day

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,17 +8,17 @@
     <title>Document</title>
     @vite('resources/css/app.css','resources/js/app.js')
 </head>
-<body class="bg-background text-text">
+<body class="bg-background text-text min-h-screen">
     <div class="px-10">
-        <nav class="p-5 mt-2 mx-[-2rem] flex items-center justify-between rounded-xl bg-accent/80 sticky top-[0.5rem]">
+        <nav class="p-5 flex items-center justify-between bg-white mx-[-2.5rem] sticky top-0 shadow-md">
             <div class="w-[300px]">
                 <a href="/">
 {{--                    <img src="{{ Vite::asset('/resources/images/logo.svg') }}" alt="logo">--}}
-                    <p class="text-3xl font-bold">SportClubPenguin</p>
+                    <p class="text-3xl font-bold text-accent">SportClubPenguin</p>
                 </a>
             </div>
             <div class="flex grow justify-center">
-                <ul class="flex items-center space-x-5 font-bold text-xl">
+                <ul class="flex items-center space-x-5 font-bold text-xl text-accent">
                     <li>
                         <a href="#">
                             О нас
@@ -31,25 +31,35 @@
                     </li>
                     <li>
                         <a href="#">
-                            Что у нас есть
+                            Акции
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            Акции
+                            Тренеры
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Новости
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Контакты
                         </a>
                     </li>
                 </ul>
             </div>
             <div class="w-[300px]">
-                <ul class="flex items-center justify-end space-x-2 text-xl font-bold">
-                    <li><a href="/">Войти</a></li>
-                    <li><a href="/">Регистрация</a></li>
+                <ul class="flex items-center justify-end text-lg font-bold">
+                    <li class="px-5 py-3 my-[-1rem] text-accent"><a href="/">Войти</a></li>
+                    <li class="px-5 py-3 my-[-1rem] bg-accent rounded-full text-background"><a href="/">Зарегистрироваться</a></li>
                 </ul>
             </div>
         </nav>
-        <main class="mt-10 max-w-[986px] mx-auto border border-gray-800">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam commodi dolor dolores eaque in ipsa itaque quod tempore! Accusamus amet architecto consequatur culpa enim est fuga, illum ipsa iste modi mollitia necessitatibus nulla ratione similique sint, temporibus vero! Consequatur dolor dolore earum eveniet laboriosam molestias, porro quaerat ratione, repellat sequi tempora vero voluptatibus? Aut consectetur doloribus exercitationem illum molestias mollitia, nostrum omnis pariatur tempora totam? Architecto aspernatur aut culpa dicta dolor dolorem doloremque, eius excepturi explicabo fuga ipsa labore laborum laudantium nemo nesciunt optio possimus provident quae quam, quidem repellendus ullam vero. Adipisci consectetur cumque eligendi et expedita natus odio. Accusamus ad alias commodi, culpa cupiditate, dolore ducimus eligendi illo molestias neque nisi perspiciatis praesentium provident quibusdam suscipit! Accusamus asperiores fugiat molestias, necessitatibus placeat reiciendis sequi tenetur vel? Alias amet at dolores error nemo non optio quam veniam voluptas voluptatem. Accusantium alias consectetur dicta dolor ex fugit, hic incidunt molestias nisi non obcaecati omnis possimus quae quaerat quam, quas quia ratione reprehenderit sit veritatis? Aliquid blanditiis distinctio est non suscipit. Consequuntur delectus dicta eos facere fugiat fugit hic impedit minus, nihil nostrum, odio quis sapiente temporibus. Accusantium ad aliquam animi consequatur dolor eos in natus obcaecati perferendis quisquam ratione, repellat! Aperiam assumenda blanditiis delectus deserunt dolor doloremque ea eaque esse facilis laborum magni minus mollitia natus nisi nobis nostrum officiis placeat possimus, praesentium quia quis rem repellat, sapiente sit sunt! Accusantium ad aliquid aperiam architecto atque, aut consequatur corporis dolores earum, et exercitationem expedita fuga hic illum iure laboriosam magni minus mollitia non nulla, obcaecati officiis quo quos repellat sed sunt tenetur veniam. Culpa cumque debitis deleniti doloremque dolorum ex, expedita laboriosam magnam pariatur placeat porro quae qui quia quisquam tenetur. Accusantium beatae consectetur delectus ducimus esse facere facilis in laboriosam nisi officiis possimus, quam quibusdam quod recusandae reprehenderit rerum saepe voluptates. Aspernatur debitis numquam officia, soluta veniam voluptates voluptatum. Ab adipisci animi at aut consequatur culpa cumque, dicta dolorem doloribus eaque eligendi eos ex excepturi expedita hic laboriosam magnam maxime neque nesciunt nisi nostrum porro quae quasi quisquam repellendus, rerum saepe similique sint soluta tenetur ullam vel voluptate voluptatum! Ea enim facilis laboriosam laudantium magni omnis repellat, unde. Deleniti, ipsa officia! Accusamus cum dolor harum iste libero nemo nisi nobis, officia officiis porro qui, tempore voluptatibus. Blanditiis corporis cum dolore eum excepturi exercitationem nostrum, perspiciatis quaerat quam. Ex nemo officiis sed voluptas. Assumenda autem commodi dolor doloribus ducimus est ex id iste molestias mollitia nemo nobis odit quae reprehenderit sunt tempora, tenetur unde ut vel vero! Accusantium aperiam aspernatur assumenda at beatae consectetur consequatur deserunt dignissimos doloribus dolorum eaque eligendi, enim et exercitationem facere id incidunt ipsa libero nobis nostrum nulla odio officia omnis placeat quae quia repellendus, reprehenderit sed sequi sint sit soluta totam veniam. Asperiores doloribus eligendi illum inventore, ipsam minus necessitatibus nesciunt nihil nisi omnis provident quaerat ratione reprehenderit, saepe temporibus ullam, vero voluptatum? Ad alias amet commodi consequuntur delectus dolore eligendi esse est excepturi expedita iusto laudantium magni minima minus, molestias nihil numquam omnis, quae quaerat quam quia quidem quo recusandae reiciendis saepe sed temporibus velit voluptate voluptatem, voluptatibus! Accusamus adipisci at aut consectetur dolorem ea earum eius, enim est facilis harum id illo inventore ipsam maiores minus modi mollitia nam nesciunt nihil nobis officiis, omnis placeat provident quasi qui quibusdam quis, quo recusandae reiciendis repellat repellendus sed sint sit velit voluptatem voluptatum. Accusamus aliquam amet aperiam architecto beatae cum cupiditate delectus dicta doloribus ducimus ea earum eum excepturi exercitationem facilis fugiat in laboriosam maxime minima molestias natus necessitatibus nihil numquam officiis pariatur perspiciatis porro quasi quia quidem, quos reprehenderit sit sunt tempora tempore veritatis vero voluptates. Ab accusamus asperiores aspernatur cum dignissimos distinctio dolorem eaque esse eveniet fugit ipsum itaque laborum magnam, nesciunt nostrum, odio odit possimus qui quis quod repudiandae soluta ut vero. Ab accusamus ad aliquid aperiam distinctio dolorem error, explicabo fugit iste itaque labore modi provident, quam quia quisquam quos repellat repudiandae rerum suscipit temporibus tenetur veritatis vitae voluptatibus. Animi aut consectetur consequuntur dolor dolores ducimus, earum est eum excepturi exercitationem harum incidunt ipsam laborum maiores neque nesciunt pariatur possimus praesentium quas qui quibusdam repellendus sed unde vel veniam vitae voluptate voluptatibus? Repudiandae unde, vero? Ab adipisci aliquam amet asperiores atque autem blanditiis cum cumque, cupiditate dignissimos doloribus eaque esse ex laboriosam maiores minima natus neque nisi nulla odit omnis quia quis quos reprehenderit repudiandae similique sint totam ullam unde ut. Amet at distinctio, ea eligendi fugit harum iure, laudantium modi nesciunt non officia rerum sapiente similique soluta temporibus. Accusamus amet aperiam blanditiis delectus deserunt est exercitationem expedita explicabo facilis fugit inventore ipsum iure labore maxime natus numquam obcaecati perspiciatis possimus, praesentium quasi recusandae vel veniam voluptate. Ad alias aspernatur autem cupiditate debitis dignissimos doloremque earum, eligendi excepturi libero molestiae nobis perspiciatis quibusdam quidem quis, ratione recusandae rem rerum sint, tenetur. Accusantium consequatur eligendi minima molestiae officiis perspiciatis quasi quibusdam, ratione recusandae repellendus? Adipisci consequuntur dignissimos doloremque dolorum nemo obcaecati porro provident, temporibus? Animi asperiores at autem culpa, dignissimos et eveniet ex in iste laboriosam laudantium mollitia, nam neque nostrum nulla. Assumenda, atque deleniti dolor dolorem eaque enim ex excepturi, illo in libero, odio omnis perspiciatis provident saepe sapiente sit totam voluptatibus. A consequuntur enim, eos expedita molestiae nisi voluptatum! Est magnam repellat soluta. Beatae blanditiis, deserunt dolores et fugiat magni molestiae molestias natus numquam ratione rem saepe sit ullam? Excepturi maiores obcaecati praesentium provident quae recusandae. A accusamus accusantium ad animi architecto consequuntur corporis, culpa dignissimos doloremque ducimus eaque est et eveniet excepturi fugiat iure magnam minima quam quo repellat repellendus rerum sapiente, sint suscipit tempora tempore unde? Cum cupiditate, dignissimos distinctio eos est non obcaecati quasi quis quo rem. Animi cum dolore dolores ex modi molestiae quia sapiente soluta tempore? A aperiam assumenda atque autem cum dolores esse exercitationem expedita minus optio quae rem reprehenderit sapiente sequi similique, suscipit, voluptatem. Accusantium dicta libero odio perferendis tempore. Aliquam asperiores autem, beatae deleniti fugit incidunt inventore iusto labore mollitia nihil odio officia perferendis perspiciatis porro provident repellat sapiente sit vel voluptatibus.
+        <main class="mt-10 max-w-[986px] mx-auto ">
+
         </main>
     </div>
 </body>
