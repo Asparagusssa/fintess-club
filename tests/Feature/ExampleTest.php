eda572168f5e865f0819d36f5daab9036be993e5
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2024-11-20 12:57:49 +0400

    add test deposit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,38 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+use App\Models\Balance;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
-    $response->assertStatus(200);
+beforeEach(function () {
+    DB::beginTransaction();
+});
+
+afterEach(function () {
+    DB::rollBack();
+});
+
+it('test deposit', function ()
+{
+    $user = User::factory()->create();
+    $user->balance()->create();
+    $this->actingAs($user);
+
+    $balance = $user->balance->amount;
+
+    $response = $this->post('/user/wallet', ['amount' => 100]);
+
+    $response->assertRedirect('/user/wallet');
+
+    $this->assertDatabaseHas('balances', [
+        'user_id' => $user->id,
+        'amount' => $balance + 100,
+    ]);
+
+    $this->assertDatabaseHas('transactions', [
+        'balance_id' => $user->balance->id,
+        'type' => 'Пополнение',
+        'amount' => 100,
+        'description' => 'Пополнение баланса'
+    ]);
 });
